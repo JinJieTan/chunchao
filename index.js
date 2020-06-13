264089d300e0f9b484194a2c5604045d1583a9cc
tanjinjie <453089136@qq.com>
2020-06-14 01:16:05 +0800

    update chunchao use

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { registryApp, start } from './src/index';
+import { registryApp, start } from 'chunchao/src/index';
 registryApp('http://localhost:8889', (location) => location.pathname === '/subapp1');
 registryApp('http://localhost:8890', (location) => location.pathname === '/subapp2');
 start();
